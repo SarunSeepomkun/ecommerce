bcdaca2302cf440e43ea139c3334ad911eb701b9
Sarun Seepomkun <sarunseepomkun@gmail.com>
2021-04-20 13:51:42 +0700

    Update

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,47 +1,79 @@
-import React from 'react';
-import { Container , Typography , Button , Grid } from '@material-ui/core';
-import useStyles from './styles';
+import React from "react";
+import { Container, Typography, Button, Grid } from "@material-ui/core";
+import useStyles from "./styles";
 import CartItem from "./CartItem/CartItem";
-import {Link} from 'react-router-dom';
+import { Link } from "react-router-dom";
 
-const Cart = ({ cart , handleUpdateCartQty , handleRemoveFromCart , handleEmptyCart }) => {
-    const classes = useStyles();
+const Cart = ({
+  cart,
+  handleUpdateCartQty,
+  handleRemoveFromCart,
+  handleEmptyCart,
+}) => {
+  const classes = useStyles();
 
-    const EmptyCart = ()=> (
-        <Typography variant="subtitle1">You don't have an item in your cart.
-            <Link to="/" className={classes.link}>Add some item</Link>
+  const EmptyCart = () => (
+    <Typography variant="subtitle1">
+      You don't have an item in your cart.
+      <Link to="/" className={classes.link}>
+        Add some item
+      </Link>
+    </Typography>
+  );
+  const FilledCart = () => (
+    <div>
+      <Grid container spacing={3}>
+        {cart.line_items.map((item) => (
+          <Grid item xs={12} sm={3} key={item.id}>
+            <CartItem
+              item={item}
+              onUpdateCartQty={handleUpdateCartQty}
+              onRemoveFromCart={handleRemoveFromCart}
+            />
+          </Grid>
+        ))}
+      </Grid>
+      <div className={classes.cardDetails}>
+        <Typography variant="h5">
+          Subtotal : {cart.subtotal.formatted_with_symbol}
         </Typography>
-    );
-    const FilledCart = ()=>(
         <div>
-            <Grid container spacing={3}>
-                { cart.line_items.map((item)=> (
-                    <Grid item xs={12} sm={4} key={item.id}>
-                        <CartItem item={item} onUpdateCartQty={handleUpdateCartQty} onRemoveFromCart={handleRemoveFromCart} />
-                    </Grid>
-                ))}
-            </Grid>
-            <div className={classes.cardDetails}>
-                <Typography variant="h4">
-                    Subtotal : {cart.subtotal.formatted_with_symbol}
-                </Typography>
-                <div>
-                    <Button className={classes.emptyButton} onClick={handleEmptyCart} size="large" type="button" variant="contained" color="secondary">Empty Cart</Button>
-                    <Button className={classes.checkoutButton} size="large" type="button" variant="contained" color="primary">Checkout</Button>
-                </div>
-            </div>
+           <Button
+            className={classes.emptyButton}
+            onClick={handleEmptyCart}
+            size="large"
+            type="button"
+            variant="contained"
+            color="secondary"
+            
+          >
+            Empty Cart
+          </Button>
+         {/* <Button
+            className={classes.checkoutButton}
+            size="large"
+            type="button"
+            variant="contained"
+            color="primary"
+          >
+            Checkout
+          </Button> */}
         </div>
-    );
+      </div>
+    </div>
+  );
 
-    if(!cart.line_items) return 'Loading...';
+  if (!cart.line_items) return "Loading...";
 
-    return (
-        <Container>
-            <div className={classes.toolbar}></div>
-            <Typography className={classes.title} variant="h3" gutterBottom>Your Shopping Cart</Typography>
-            { !cart.line_items.length  ? <EmptyCart /> : <FilledCart />}
-        </Container>
-    )
-}
+  return (
+    <Container>
+      <div className={classes.toolbar}></div>
+      <Typography className={classes.title} variant="h5" gutterBottom>
+        Your Shopping Cart
+      </Typography>
+      {!cart.line_items.length ? <EmptyCart /> : <FilledCart />}
+    </Container>
+  );
+};
 
-export default Cart
+export default Cart;
--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -11,7 +11,7 @@ const Products = ({ products , onAddToCart }) => {
       <div className={classes.toolbar}></div>
       <Grid container justify="center" spacing={4}>
         {products.map((product) => (
-          <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
+          <Grid item key={product.id} xs={12} sm={6} md={3}>
             <Product product={product} onAddToCart={onAddToCart} />
           </Grid>
         ))}
